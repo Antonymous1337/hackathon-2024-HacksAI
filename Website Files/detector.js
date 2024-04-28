4ab71684a7af899a8fb7936edc17e7dfd714767b
Galaphyn <96713558+Galaphyn@users.noreply.github.com>
2024-04-28 04:34:49 -0700

    puuuuush

--- Website Files/detector.js
+++ Website Files/detector.js
@@ -1,12 +1,55 @@
 const site = window.location.hostname;
 
-//class AI{}
-//class Real{}
+class ScanObject
+{
 
+  #loading = true
+  #pass = false
+  #percentage = -1
+  constructor(src, element)
+  {
+    this.#loading = true;
+    this.#pass = false;
+    this.#percentage = -1;
+    alert("G")
+    //#checkifreal(src)
+    checkifrealprototype(element)
+    //private setTooltip()
+  }
+
+  /*
+  #checkIfReal(src)
+  {
+    //Check class of image. if "real" then return true, else false
+    if (element instanceof Real)
+    {
+      return false;
+      //Maybe change tooltip?
+    }
+    else
+    {
+      return true;
+      //Change tooltip?
+    }
+  }
+  */
+
+  #checkifrealprotype(element)
+  {
+    alert(element.className);
+    //if (element.className) {}
+  }
+
+
+}
+
+let scanList = [];
 var element;
 var isImage = false;
 var isHover = false;
 
+
+//passfail percentage
 $(window).on('mouseenter', function(e1)
 {
     var x = e1.clientX, y = e1.clientY;
@@ -34,28 +77,15 @@ $(this).on('keypress', function(event) {
   //alert(event.keyCode);
   if (event.keyCode == 115 && isHover == true && isImage == true) //https://www.toptal.com/developers/keycode/table
   {
-    alert('Its an image!');
+      // spawn object
+
+      //get image src attribute
+      let src = element.src
+      scanList.push(new ScanObject(src, element))
+
+
+
+    //alert('Its an image!');
     //checkIfReal(element);
   }
-  else if (event.keyCode == 115) //If prev is false but the key was right
-  {
-  	//alert('Its not an image!');
-  }
 })
-
-/*
-function checkIfReal(element)
-{
-  //Check class of image. if "real" then return true, else false
-  if (element instanceof Real)
-  {
-    return false;
-    //Maybe change tooltip?
-  }
-  else
-  {
-    return true;
-    //Change tooltip?
-  }
-}
-*/
