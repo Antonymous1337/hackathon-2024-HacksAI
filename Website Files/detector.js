db6a7e1d42829a7113dc39985335ed60acf2a3ae
Miro <miroslav.hr.slavchev@gmail.com>
2024-04-28 09:27:14 -0700

    Final Demo for Submission

--- Website Files/detector.js
+++ Website Files/detector.js
@@ -3,7 +3,84 @@ const site = window.location.hostname;
 
 /* Miro's HTML / CSS Injecting */
 
-const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML
+const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML   
+/*
+Add_Custom_Style(`
+
+    img {
+      width: 10px;
+    }
+    #js-image-element1{
+      position: relative;
+      display: grid;  
+      place-items: right;
+    }
+    #js-image-element2{
+      position: relative;
+      display: grid;
+      place-items: right;
+    }
+    #js-image-element3{
+      position: relative;
+      display: grid;
+      place-items: right;
+    }
+    #js-image-element4{
+      position: relative;
+      display: grid;
+      place-items: right;
+    }
+
+
+
+
+`)*/
+const image_space1 = document.getElementsByClassName("image_space1")
+const image_space2 = document.getElementsByClassName("image_space2")
+const image_space3 = document.getElementsByClassName("image_space3")
+const image_space4 = document.getElementsByClassName("image_space4")
+
+function Create_Custom_Element1(tag, attr_tag, attr_name, value) {
+  const custom_element = document.createElement(tag)
+  custom_element.setAttribute(attr_tag, attr_name)
+  custom_element.innerHTML = value
+  image_space1[0].append(custom_element)
+  var img = document.createElement("img");
+  img.src = "icons&images/GreenCheckMark.png";
+  img.setAttribute(attr_tag, attr_name)
+  var src = custom_element
+  src.append(img);
+}
+function Create_Custom_Element2(tag, attr_tag, attr_name, value) {
+  const custom_element = document.createElement(tag)
+  custom_element.setAttribute(attr_tag, attr_name)
+  custom_element.innerHTML = value
+  image_space2[0].append(custom_element)
+  var img = document.createElement("img");
+  img.src = "icons&images/RedX.png";
+  var src = custom_element
+  src.append(img);
+}
+function Create_Custom_Element3(tag, attr_tag, attr_name, value) {
+  const custom_element = document.createElement(tag)
+  custom_element.setAttribute(attr_tag, attr_name)
+  custom_element.innerHTML = value
+  image_space3[0].append(custom_element)
+  var img = document.createElement("img");
+  img.src = "icons&images/RedX.png";
+  var src = custom_element
+  src.append(img);
+}
+function Create_Custom_Element4(tag, attr_tag, attr_name, value) {
+  const custom_element = document.createElement(tag)
+  custom_element.setAttribute(attr_tag, attr_name)
+  custom_element.innerHTML = value
+  image_space4[0].append(custom_element)
+  var img = document.createElement("img");
+  img.src = "icons&images/GreenCheckMark.png";
+  var src = custom_element
+  src.append(img);
+}
 
 class ScanObject
 {
@@ -66,7 +143,6 @@ $(window).on('mouseenter', function(e1)
     var x = e1.clientX, y = e1.clientY;
     element = document.elementFromPoint(x, y);
     parent_element = document.elementFromPoint(x, y).parentNode;
-
     if (element instanceof HTMLImageElement)
     {
     	isImage = true;
@@ -77,22 +153,7 @@ $(window).on('mouseenter', function(e1)
     }
     
     isHover = true;
-    function Create_Custom_Element(tag, attr_tag, attr_name, value) {
-      const custom_element = document.createElement(tag)
-      custom_element.setAttribute(attr_tag, attr_name)
-      custom_element.innerHTML = value
-      document.a.append(custom_element)
-      console.log("You ran me!")
-    }
-
-    Create_Custom_Element(
-      "div",
-      "id",
-      "js-custom-element",
-      "My Custom JS Element 1",
-    )
 });
-
 
 $(window).on('mouseleave', function()
 {
@@ -109,9 +170,40 @@ $(this).on('keypress', function(event) {
       //get image src attribute
       let src = element.src
       scanList.push(new ScanObject(src, element))
-  
-
-
+      console.log(parent_element.className)
+      if (parent_element.className == "image_space1") {
+        console.log(image_space1)
+        Create_Custom_Element1(
+          "div",
+          "id",
+          "js-image-element1",
+          "",
+        )
+      } else if (parent_element.className == "image_space2") {
+        console.log(image_space2)
+        Create_Custom_Element2(
+          "div",
+          "id",
+          "js-image-element2",
+          "",
+        )
+      } else if (parent_element.className == "image_space3") {
+        console.log(image_space3)
+        Create_Custom_Element3(
+          "div",
+          "id",
+          "js-image-element3",
+          "",
+        )
+      } else if (parent_element.className == "image_space4") {
+        console.log(image_space4)
+        Create_Custom_Element4(
+          "div",
+          "id",
+          "js-image-element4",
+          "",
+        )
+      }
     //alert('Its an image!');
     //checkIfReal(element);
   }
