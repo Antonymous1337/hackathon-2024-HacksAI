7e4b023f798fdbe3abcc66b2999a295124f8e47f
Galaphyn <rorymb10@gmail.com>
2024-05-26 21:26:17 -0700

    expand on hover!!!!! Wozer

--- Website Files/detector.js
+++ Website Files/detector.js
@@ -7,7 +7,21 @@ const site = window.location.hostname;
 
 /* Miro's HTML / CSS Injecting */
 
-const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML   
+//const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML   
+
+const Add_Custom_Style = css => {
+  const style = document.createElement("style");
+  style.innerHTML = css;
+  document.head.appendChild(style);
+}
+
+Add_Custom_Style(`
+  .expand-on-hover:hover {
+    transform: scale(1.2);
+    transition: transform 0.3s ease-in-out;
+  }
+`);
+
 /*
 Add_Custom_Style(`
 
@@ -34,12 +48,8 @@ Add_Custom_Style(`
       display: grid;
       place-items: right;
     }
-
-
-
-
 `)*/
-
+/*
 function Create_Custom_Element(tag, attr_tag, attr_name, value) {
   
   const custom_element = document.createElement(tag)
@@ -66,7 +76,7 @@ function Create_Custom_Element(tag, attr_tag, attr_name, value) {
   var src = custom_element
   src.append(img);
 }
-
+*/
 /*
 const image_space1 = document.getElementsByClassName("image_space1")
 const image_space2 = document.getElementsByClassName("image_space2")
@@ -153,13 +163,28 @@ class ScanObject
       img.src = chrome.runtime.getURL('/icons&images/RedX.png')
     }
   
+    //Adds expand on hover to image
+    img.className = "expand-on-hover";
+
     //Change size and set position of image to the top right of image.
     img.style.width = "50px";
     img.style.height = "50px";
     img.style.position = "absolute"; //position absolute aligns image relative with nearest ancestor?
     img.style.top = "0";
     img.style.right = "0";
-  
+
+    /*
+    //Domain Expansion: Add functions to img that trigger on mouseover/out to expand widtch and hegths
+    img.onmouseover = function() {
+      this.style.width = "70px";
+      this.style.height = "70px";
+    }
+    img.onmouseout = function() {
+      this.style.width = "50px";
+      this.style.height = "50px";
+    }
+    */
+
     //Apply image to parent element
     parent_element.append(img);
 
@@ -207,7 +232,11 @@ $(window).on('mouseenter', function(e1)
 {
     var x = e1.clientX, y = e1.clientY;
     element = document.elementFromPoint(x, y);
-    parent_element = element.parentNode;
+    if (element) //Make sure its not null to fix error with trying to get parent of null element
+    {
+      parent_element = element.parentNode;
+    }
+
     //If imageElement and isnt just a small image like a logo
     if (element instanceof HTMLImageElement && element.naturalWidth > 100 && element.naturalHeight > 100)
     {
